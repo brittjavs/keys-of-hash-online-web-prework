2b1ea9ca9e877df31e81398dd417e1ac564a737d
Brittany Javalera <brittjavalera@gmail.com>
2019-06-06 22:05:01 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,6 +6,7 @@ class Hash
   end
 end
 end
+end
 
 # (:animals)
 # {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
